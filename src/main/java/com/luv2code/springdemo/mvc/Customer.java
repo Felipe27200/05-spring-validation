18eacc5dd9b959472928b210518472dfff3faf08
juan.zea <juan.zea@innclod.com>
2023-10-09 20:32:06 -0500

    Set a range of value and handle the null value

--- src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,6 +14,23 @@ public class Customer {
     @NotNull(message = "Is required")
     @Size(min = 1, message = "Is required") // Set the minimum length
     private String lastName;
+
+    @NotNull(message = "Is required")
+    @Min(value = 0, message = "Must be greater than or equal to zero")
+    @Max(value = 10, message = "Must be less than or equal to ten")
+    //private int freePasses;
+
+    /*
+    * +------------------------------+
+    * | MAKE AN INPUT FIELD REQUIRED |
+    * +------------------------------+
+    *
+    * We don't have to use int because is a primitive type,
+    * so we must use the related class to the type, because
+    * if the value is sent with anything the trim method converted
+    * into null, so this VI can save null as a reference to nothing.
+    * */
+    private Integer freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -27,5 +46,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
     }
 }
